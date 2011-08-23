f6eb4a9e7b6da5656eccc5cfadc5f5488106815b
thorsten-k <t.kisner@web.de>
2011-08-23 13:41:15 +0000

    Category added

--- xml/src/main/java/net/sf/otrcutmp4/model/xml/series/Category.java
+++ xml/src/main/java/net/sf/otrcutmp4/model/xml/series/Category.java
@@ -6,18 +6,17 @@
 //
 
 
-package net.sf.otrcutmp4.model.xml.otr;
+package net.sf.otrcutmp4.model.xml.series;
 
 import java.io.Serializable;
 import java.util.ArrayList;
 import java.util.List;
 import javax.xml.bind.annotation.XmlAccessType;
 import javax.xml.bind.annotation.XmlAccessorType;
+import javax.xml.bind.annotation.XmlAttribute;
 import javax.xml.bind.annotation.XmlElement;
 import javax.xml.bind.annotation.XmlRootElement;
 import javax.xml.bind.annotation.XmlType;
-import net.sf.otrcutmp4.model.xml.series.Category;
-import net.sf.otrcutmp4.model.xml.series.Series;
 
 
 /**
@@ -31,8 +30,9 @@ import net.sf.otrcutmp4.model.xml.series.Series;
  *     &lt;restriction base="{http://www.w3.org/2001/XMLSchema}anyType">
  *       &lt;sequence>
  *         &lt;element ref="{http://otrcutmp4.sf.net/series}series" maxOccurs="unbounded"/>
- *         &lt;element ref="{http://otrcutmp4.sf.net/series}category" maxOccurs="unbounded"/>
  *       &lt;/sequence>
+ *       &lt;attribute name="id" type="{http://www.w3.org/2001/XMLSchema}long" />
+ *       &lt;attribute name="name" type="{http://www.w3.org/2001/XMLSchema}string" />
  *     &lt;/restriction>
  *   &lt;/complexContent>
  * &lt;/complexType>
@@ -42,19 +42,20 @@ import net.sf.otrcutmp4.model.xml.series.Series;
  */
 @XmlAccessorType(XmlAccessType.FIELD)
 @XmlType(name = "", propOrder = {
-    "series",
-    "category"
+    "series"
 })
-@XmlRootElement(name = "otr")
-public class Otr
+@XmlRootElement(name = "category")
+public class Category
     implements Serializable
 {
 
     private final static long serialVersionUID = 1L;
-    @XmlElement(namespace = "http://otrcutmp4.sf.net/series", required = true)
+    @XmlElement(required = true)
     protected List<Series> series;
-    @XmlElement(namespace = "http://otrcutmp4.sf.net/series", required = true)
-    protected List<Category> category;
+    @XmlAttribute(name = "id")
+    protected Long id;
+    @XmlAttribute(name = "name")
+    protected String name;
 
     /**
      * Gets the value of the series property.
@@ -94,40 +95,63 @@ public class Otr
     }
 
     /**
-     * Gets the value of the category property.
+     * Gets the value of the id property.
      * 
-     * <p>
-     * This accessor method returns a reference to the live list,
-     * not a snapshot. Therefore any modification you make to the
-     * returned list will be present inside the JAXB object.
-     * This is why there is not a <CODE>set</CODE> method for the category property.
-     * 
-     * <p>
-     * For example, to add a new item, do as follows:
-     * <pre>
-     *    getCategory().add(newItem);
-     * </pre>
-     * 
-     * 
-     * <p>
-     * Objects of the following type(s) are allowed in the list
-     * {@link Category }
-     * 
-     * 
+     * @return
+     *     possible object is
+     *     {@link Long }
+     *     
      */
-    public List<Category> getCategory() {
-        if (category == null) {
-            category = new ArrayList<Category>();
-        }
-        return this.category;
+    public long getId() {
+        return id;
     }
 
-    public boolean isSetCategory() {
-        return ((this.category!= null)&&(!this.category.isEmpty()));
+    /**
+     * Sets the value of the id property.
+     * 
+     * @param value
+     *     allowed object is
+     *     {@link Long }
+     *     
+     */
+    public void setId(long value) {
+        this.id = value;
     }
 
-    public void unsetCategory() {
-        this.category = null;
+    public boolean isSetId() {
+        return (this.id!= null);
+    }
+
+    public void unsetId() {
+        this.id = null;
+    }
+
+    /**
+     * Gets the value of the name property.
+     * 
+     * @return
+     *     possible object is
+     *     {@link String }
+     *     
+     */
+    public String getName() {
+        return name;
+    }
+
+    /**
+     * Sets the value of the name property.
+     * 
+     * @param value
+     *     allowed object is
+     *     {@link String }
+     *     
+     */
+    public void setName(String value) {
+        this.name = value;
+    }
+
+    public boolean isSetName() {
+        return (this.name!= null);
     }
 
 }
