9a35969679246f53d817fa0194eed9c80e3c0fd5
thorsten-k <thorsten-k@16432bf1-0806-4b2d-aae0-48c465c8950a>
2011-08-24 08:10:11 +0000

    Exception added

--- trunk/util/src/main/java/net/sf/otrcutmp4/controller/interfaces/rest/OtrRestSeriesService.java
+++ trunk/util/src/main/java/net/sf/otrcutmp4/controller/interfaces/rest/OtrRestSeriesService.java
@@ -1,6 +1,8 @@
 package net.sf.otrcutmp4.controller.interfaces.rest;
 
 import net.sf.otrcutmp4.model.xml.otr.Otr;
+import net.sf.otrcutmp4.model.xml.series.Category;
+import net.sf.otrcutmp4.model.xml.series.Episode;
 import net.sf.otrcutmp4.model.xml.series.Series;
 import net.sf.otrcutmp4.model.xml.series.Tag;
 import net.sf.otrcutmp4.model.xml.series.Tags;
@@ -12,5 +14,10 @@ public interface OtrRestSeriesService
 	public Tag tag(long episodeId, String otrName);
 	
 	public Series series(long seriesId);
+	
 	public Otr allSeries();
+	
+	public Category addCategory(Category category);
+	public Series addSeries(Series series);
+	public Episode addEpisode(Episode episode);
 }
