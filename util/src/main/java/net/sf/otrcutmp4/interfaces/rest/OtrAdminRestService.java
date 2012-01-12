ccc4d39dac8f8a606ddbd65a89de18bce262858f
thorsten-k <t.kisner@web.de>
2012-01-12 11:46:53 +0000

    Admin REST throws with exception

--- util/src/main/java/net/sf/otrcutmp4/interfaces/rest/OtrAdminRestService.java
+++ util/src/main/java/net/sf/otrcutmp4/interfaces/rest/OtrAdminRestService.java
@@ -1,12 +1,13 @@
 package net.sf.otrcutmp4.interfaces.rest;
 
+import net.sf.ahtutils.exception.processing.UtilsProcessingException;
 import net.sf.otrcutmp4.model.xml.otr.Format;
 import net.sf.otrcutmp4.model.xml.otr.Quality;
 import net.sf.otrcutmp4.model.xml.series.Series;
 
 public interface OtrAdminRestService
 {
-	Series addSeries(Series series);
-	Format addFormat(Format format);
-	Quality addQuality(Quality quality);
+	Series addSeries(Series series) throws UtilsProcessingException;
+	Format addFormat(Format format) throws UtilsProcessingException;
+	Quality addQuality(Quality quality) throws UtilsProcessingException;
 }
