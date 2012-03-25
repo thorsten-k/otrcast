fdadb4e47488ab75d2c58a9d32609f6146cfb8f3
thorsten-k <t.kisner@web.de>
2012-03-25 22:30:07 +0000

    LinkListParser fixed

--- util/src/test/java/net/sf/otrcutmp4/controller/processor/exlp/TestLinklistConverter.java
+++ util/src/test/java/net/sf/otrcutmp4/controller/processor/exlp/TestLinklistConverter.java
@@ -6,12 +6,15 @@ import java.io.FileNotFoundException;
 import net.sf.exlp.util.exception.ExlpConfigurationException;
 import net.sf.exlp.util.io.StringIO;
 import net.sf.exlp.util.xml.JaxbUtil;
+import net.sf.otrcutmp4.controller.processor.exlp.parser.LinkListParser;
 import net.sf.otrcutmp4.model.xml.otr.Linklist;
 import net.sf.otrcutmp4.test.AbstractUtilTest;
 import net.sf.otrcutmp4.test.OtrUtilTstBootstrap;
 
 import org.apache.commons.configuration.Configuration;
+import org.junit.Assert;
 import org.junit.BeforeClass;
+import org.junit.Ignore;
 import org.junit.Test;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -20,31 +23,44 @@ public class TestLinklistConverter extends AbstractUtilTest
 {
 	final static Logger logger = LoggerFactory.getLogger(TestLinklistConverter.class);
 	
-	private static File fXml;
-	private static String sourceLinklist;
+	private static String srcLinklist;
+	private static File dstXml;
+	
 	
 	@BeforeClass
 	public static void initFiles()
 	{
-		fXml = new File("src/test/resources/xml","linklist.xml");
-		sourceLinklist = StringIO.loadTxt("src/test/resources/data/linklist.txt");
+		srcLinklist = StringIO.loadTxt("src/test/resources/data/linklist.txt");
+		dstXml = new File("src/test/resources/xml","linklist.xml");
 	}
     
     @Test
     public void testConverter() throws FileNotFoundException
     {
     	LinklistConverter llc = new LinklistConverter();
-    	Linklist actual = llc.convert(sourceLinklist);
-    	Linklist expected = JaxbUtil.loadJAXB(fXml.getAbsolutePath(), Linklist.class);
+    	Linklist actual = llc.convert(srcLinklist);
+    	Linklist expected = JaxbUtil.loadJAXB(dstXml.getAbsolutePath(), Linklist.class);
     	assertJaxbEquals(expected, actual);
     }
+    
+    @Test
+    public void testNormal() throws FileNotFoundException
+    {
+    	LinklistConverter llc = new LinklistConverter();
+    	Linklist actual = llc.convert(srcLinklist);
+    	Assert.assertEquals("Not only 3 download type",3, actual.getDownload().size());
+    	Assert.assertEquals("Not normal type",LinkListParser.secNormal, actual.getDownload().get(0).getType());
+    	Assert.assertEquals("Not normal type",LinkListParser.secPrio, actual.getDownload().get(1).getType());
+    	Assert.assertEquals("Not normal type",LinkListParser.secPay, actual.getDownload().get(2).getType());
+    }
+    
  
     public void save()
     {
     	LinklistConverter llc = new LinklistConverter();
-    	Linklist xml = llc.convert(sourceLinklist);
+    	Linklist xml = llc.convert(srcLinklist);
     	JaxbUtil.debug(this.getClass(),xml);
-    	JaxbUtil.save(fXml, xml, nsPrefixMapper, true);
+    	JaxbUtil.save(dstXml, xml, nsPrefixMapper, true);
     }
 	
 	public static void main(String[] args) throws ExlpConfigurationException
