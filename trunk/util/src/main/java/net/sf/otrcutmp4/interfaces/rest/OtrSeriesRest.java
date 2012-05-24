d7146d24a837edb3d28ab159f7c75f2601e6368c
thorsten-k <thorsten-k@16432bf1-0806-4b2d-aae0-48c465c8950a>
2012-05-24 08:42:52 +0000

    Method added to interface (OTR-33)

--- trunk/util/src/main/java/net/sf/otrcutmp4/interfaces/rest/OtrSeriesRest.java
+++ trunk/util/src/main/java/net/sf/otrcutmp4/interfaces/rest/OtrSeriesRest.java
@@ -1,18 +1,25 @@
 package net.sf.otrcutmp4.interfaces.rest;
 
-import net.sf.otrcutmp4.model.xml.container.Otr;
-import net.sf.otrcutmp4.model.xml.series.Category;
-import net.sf.otrcutmp4.model.xml.series.Episode;
-import net.sf.otrcutmp4.model.xml.series.Series;
-import net.sf.otrcutmp4.model.xml.series.Tag;
+import javax.ws.rs.Consumes;
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+
 import net.sf.otrcutmp4.model.xml.series.Tags;
 
+@Path("/rest/series")
 public interface OtrSeriesRest
 {
-	public Tags getTags(String otrId);
-	public Tag tag(long episodeId, String otrName);
+	@GET @Path("/tags/{id}")
+	@Produces(MediaType.APPLICATION_XML)
+	@Consumes(MediaType.TEXT_PLAIN)
+	Tags getTags(@PathParam("id") String fileName);
 	
-	public Otr allSeries();
-	public Series series(long seriesId);
+//	public Tag tag(long episodeId, String otrName);
+	
+//	public Otr allSeries();
+//	public Series series(long seriesId);
 	
 }
