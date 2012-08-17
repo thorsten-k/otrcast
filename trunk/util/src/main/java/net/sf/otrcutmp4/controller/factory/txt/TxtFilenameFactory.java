b3de2edc3958fae4622ed4b4901a30601dd70cab
thorsten-k <thorsten-k@16432bf1-0806-4b2d-aae0-48c465c8950a>
2012-08-17 17:16:15 +0000

    TxtFilenameFactory added (OTR-54)

--- trunk/util/src/main/java/net/sf/otrcutmp4/controller/factory/txt/TxtFilenameFactory.java
+++ trunk/util/src/main/java/net/sf/otrcutmp4/controller/factory/txt/TxtFilenameFactory.java
@@ -1,18 +1,40 @@
 package net.sf.otrcutmp4.controller.factory.txt;
 
+import java.util.Date;
+
 import net.sf.otrcutmp4.controller.exception.OtrProcessingException;
 import net.sf.otrcutmp4.model.xml.cut.CutList;
 
+import org.joda.time.DateTime;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 public class TxtFilenameFactory
 {
 	final static Logger logger = LoggerFactory.getLogger(TxtFilenameFactory.class);
-	
-	public TxtFilenameFactory()
+
+	public static String build(String name, String channel, int duration, Date airTime)
 	{
+		DateTime dt = new DateTime(airTime);
 		
+		StringBuffer sb = new StringBuffer();
+		sb.append(name).append("_");
+		sb.append(dt.getYearOfCentury()).append(".");
+		sb.append(tZ(dt.getMonthOfYear())).append(".");
+		sb.append(tZ(dt.getDayOfMonth()));
+		sb.append("_");
+		sb.append(tZ(dt.getHourOfDay())).append("-").append(tZ(dt.getMinuteOfHour()));
+		sb.append("_");
+		sb.append(channel).append("_").append(duration).append("_");
+		sb.append("TVOON_DE.mpg.HQ.avi");
+		return sb.toString();
+	}
+	
+	private static String tZ(int in)
+	{
+		Integer i = new Integer(in);
+		if(i<10){return "0"+in;}
+		else{return ""+in;}
 	}
 	
 	public String create(CutList cl) throws OtrProcessingException
