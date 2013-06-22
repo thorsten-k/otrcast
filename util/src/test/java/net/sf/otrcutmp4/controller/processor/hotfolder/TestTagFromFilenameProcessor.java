f0fcfbf7cb5e2f5d7285191b49dbc26f9bdcd004
thorsten-k <t.kisner@web.de>
2013-06-22 12:37:00 +0000

    test for camel processor added (OTR-82)

--- util/src/test/java/net/sf/otrcutmp4/controller/processor/hotfolder/TestTagFromFilenameProcessor.java
+++ util/src/test/java/net/sf/otrcutmp4/controller/processor/hotfolder/TestTagFromFilenameProcessor.java
@@ -1,17 +1,18 @@
-package net.sf.otrcutmp4.controller.tagger;
+package net.sf.otrcutmp4.controller.processor.hotfolder;
 
 import java.io.File;
 
 import net.sf.otrcutmp4.controller.processor.hotfolder.TagFromFilenameProcessor;
+import net.sf.otrcutmp4.controller.tagger.Mp4Tagger;
 import net.sf.otrcutmp4.model.xml.series.Episode;
 import net.sf.otrcutmp4.test.AbstractUtilTest;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-public class TestEpisodeFromFilename extends AbstractUtilTest{
+public class TestTagFromFilenameProcessor extends AbstractUtilTest{
 	
-	final static Logger logger = LoggerFactory.getLogger(TestEpisodeFromFilename.class);
+	final static Logger logger = LoggerFactory.getLogger(TestTagFromFilenameProcessor.class);
 	
 	public static void main(String[] args) throws Exception
 	{
--- util/src/test/java/net/sf/otrcutmp4/test/AbstractUtilTest.java
+++ util/src/test/java/net/sf/otrcutmp4/test/AbstractUtilTest.java
@@ -1,29 +1,21 @@
 package net.sf.otrcutmp4.test;
 
-import java.util.Date;
 import java.util.Random;
 
-import net.sf.exlp.util.DateUtil;
+import net.sf.ahtutils.test.AbstractAhtUtilsTest;
 import net.sf.exlp.util.io.LoggerInit;
 import net.sf.exlp.util.xml.JaxbUtil;
 import net.sf.otrcutmp4.model.xml.OtrCutNsPrefixMapper;
 
-import org.junit.Assert;
 import org.junit.BeforeClass;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-public abstract class AbstractUtilTest
+public abstract class AbstractUtilTest extends AbstractAhtUtilsTest
 {
 	final static Logger logger = LoggerFactory.getLogger(AbstractUtilTest.class);
 	
 	protected static Random rnd;
-	
-	@BeforeClass
-	public static void initRnd()
-	{
-		rnd = new Random();
-	}
 	
 	@BeforeClass
 	public static void initPrefixMapper()
@@ -35,17 +27,7 @@ public abstract class AbstractUtilTest
     public static void initLogger()
 	{
 		LoggerInit loggerInit = new LoggerInit("log4junit.xml");	
-		loggerInit.addAltPath("otrcutmp4-util.test");
+		loggerInit.addAltPath("config.otrcutmp4-util.test");
 		loggerInit.init();
     }
-	
-	protected void assertJaxbEquals(Object ref, Object test)
-	{
-		Assert.assertEquals(JaxbUtil.toString(ref),JaxbUtil.toString(test));
-	}
-	
-	protected static Date getDefaultDate()
-	{
-		return DateUtil.getDateFromInt(2011, 11, 11, 11, 11, 11);
-	}
 }
