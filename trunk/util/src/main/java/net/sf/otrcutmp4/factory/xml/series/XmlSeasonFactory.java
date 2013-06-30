5562bea4f46ece29965ae8cace3e47a3bb45aaf4
thorsten-k <thorsten-k@16432bf1-0806-4b2d-aae0-48c465c8950a>
2013-06-30 20:22:13 +0000

    XmlFactories now with generics (OTR-86)

--- trunk/util/src/main/java/net/sf/otrcutmp4/factory/xml/series/XmlSeasonFactory.java
+++ trunk/util/src/main/java/net/sf/otrcutmp4/factory/xml/series/XmlSeasonFactory.java
@@ -1,18 +1,20 @@
 package net.sf.otrcutmp4.factory.xml.series;
 
+import net.sf.otrcutmp4.interfaces.model.Episode;
+import net.sf.otrcutmp4.interfaces.model.Season;
+import net.sf.otrcutmp4.interfaces.model.Series;
 import net.sf.otrcutmp4.model.xml.otr.Query;
-import net.sf.otrcutmp4.model.xml.series.Season;
 
-public class XmlSeasonFactory
+public class XmlSeasonFactory<SERIES extends Series<SERIES,SEASON,EPISODE>, SEASON extends Season<SERIES,SEASON,EPISODE>,EPISODE extends Episode<SERIES,SEASON,EPISODE>>
 {	
-	private Season q;
+	private net.sf.otrcutmp4.model.xml.series.Season q;
 	
 	public XmlSeasonFactory(Query query){this(query.getSeason());}
-	public XmlSeasonFactory(Season q){this.q=q;}
+	public XmlSeasonFactory(net.sf.otrcutmp4.model.xml.series.Season q){this.q=q;}
 	
-	public Season build(net.sf.otrcutmp4.interfaces.model.Season ejb)
+	public net.sf.otrcutmp4.model.xml.series.Season build(Season<SERIES,SEASON,EPISODE> ejb)
 	{
-		Season xml = new Season();
+		net.sf.otrcutmp4.model.xml.series.Season xml = new net.sf.otrcutmp4.model.xml.series.Season();
 		if(q.isSetNr()){xml.setNr(ejb.getNr());}
 		if(q.isSetName()){xml.setName(ejb.getName());}
 		
--- trunk/util/src/main/java/net/sf/otrcutmp4/factory/xml/series/XmlSeriesFactory.java
+++ trunk/util/src/main/java/net/sf/otrcutmp4/factory/xml/series/XmlSeriesFactory.java
@@ -1,40 +1,42 @@
 package net.sf.otrcutmp4.factory.xml.series;
 
+import net.sf.otrcutmp4.interfaces.model.Episode;
+import net.sf.otrcutmp4.interfaces.model.Season;
+import net.sf.otrcutmp4.interfaces.model.Series;
 import net.sf.otrcutmp4.model.xml.otr.Query;
-import net.sf.otrcutmp4.model.xml.series.Series;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-public class XmlSeriesFactory
+public class XmlSeriesFactory<SERIES extends Series<SERIES,SEASON,EPISODE>, SEASON extends Season<SERIES,SEASON,EPISODE>,EPISODE extends Episode<SERIES,SEASON,EPISODE>>
 {	
 	final static Logger logger = LoggerFactory.getLogger(XmlSeriesFactory.class);
 	
-	private Series q;
+	private net.sf.otrcutmp4.model.xml.series.Series q;
 	
 	public XmlSeriesFactory(Query query){this(query.getSeries());}
-	public XmlSeriesFactory(Series q){this.q=q;}
+	public XmlSeriesFactory(net.sf.otrcutmp4.model.xml.series.Series q){this.q=q;}
 	
-	public Series create(String format)
+	public net.sf.otrcutmp4.model.xml.series.Series create(String format)
 	{
-		Series xml = new Series();
+		net.sf.otrcutmp4.model.xml.series.Series xml = new net.sf.otrcutmp4.model.xml.series.Series();
 		
 		return xml;
 	}
 	
-	public Series build(net.sf.otrcutmp4.interfaces.model.Series ejb)
+	public net.sf.otrcutmp4.model.xml.series.Series build(Series<SERIES,SEASON,EPISODE> ejb)
 	{
-		Series xml = new Series();
+		net.sf.otrcutmp4.model.xml.series.Series xml = new net.sf.otrcutmp4.model.xml.series.Series();
 		if(q.isSetId()){xml.setId(ejb.getId());}
 		if(q.isSetName()){xml.setName(ejb.getName());}
 		if(q.isSetKey()){xml.setKey(ejb.getCode());}
 		
 		if(q.isSetSeason())
 		{
-			XmlSeasonFactory f = new XmlSeasonFactory(q.getSeason().get(0));
-//			for(net.sf.otrcutmp4.interfaces.model.Season season : ejb.getSeasons())
+			XmlSeasonFactory<SERIES,SEASON,EPISODE> f = new XmlSeasonFactory<SERIES,SEASON,EPISODE>(q.getSeason().get(0));
+			for(Season<SERIES,SEASON,EPISODE> season : ejb.getSeasons())
 			{
-				
+				xml.getSeason().add(f.build(season));
 			}
 		}
 		
