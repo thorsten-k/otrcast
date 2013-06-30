b385200f3d97f0b09fdad0fb18833cecb4886ffb
thorsten-k <t.kisner@web.de>
2013-06-30 19:10:09 +0000

    XmlSeriesFactory activated (OTR-86)

--- util/src/main/java/net/sf/otrcutmp4/factory/xml/series/XmlSeriesFactory.java
+++ util/src/main/java/net/sf/otrcutmp4/factory/xml/series/XmlSeriesFactory.java
@@ -1,5 +1,6 @@
 package net.sf.otrcutmp4.factory.xml.series;
 
+import net.sf.otrcutmp4.model.xml.otr.Query;
 import net.sf.otrcutmp4.model.xml.series.Series;
 
 import org.slf4j.Logger;
@@ -8,13 +9,27 @@ import org.slf4j.LoggerFactory;
 public class XmlSeriesFactory
 {	
 	final static Logger logger = LoggerFactory.getLogger(XmlSeriesFactory.class);
-		
-	public static Series create(String format)
+	
+	private Series q;
+	
+	public XmlSeriesFactory(Query query){this(query.getSeries());}
+	public XmlSeriesFactory(Series q){this.q=q;}
+	
+	public Series create(String format)
 	{
 		Series xml = new Series();
 		
 		return xml;
 	}
 	
+	public Series build(net.sf.otrcutmp4.interfaces.model.Series ejb)
+	{
+		Series xml = new Series();
+		if(q.isSetId()){xml.setId(ejb.getId());}
+		if(q.isSetName()){xml.setName(ejb.getName());}
+		if(q.isSetKey()){xml.setKey(ejb.getCode());}
+		
+		return xml;
+	}
 	
 }
