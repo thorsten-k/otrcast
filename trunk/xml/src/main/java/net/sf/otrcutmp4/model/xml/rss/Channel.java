cd2349ec4ac54cb35590aaa823a45028f97d7683
thorsten-k <thorsten-k@16432bf1-0806-4b2d-aae0-48c465c8950a>
2013-07-07 13:36:05 +0000

    RSS feed XML: item (OTR-92)

--- trunk/xml/src/main/java/net/sf/otrcutmp4/model/xml/rss/Channel.java
+++ trunk/xml/src/main/java/net/sf/otrcutmp4/model/xml/rss/Channel.java
@@ -2,6 +2,8 @@
 package net.sf.otrcutmp4.model.xml.rss;
 
 import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
 import javax.xml.bind.annotation.XmlAccessType;
 import javax.xml.bind.annotation.XmlAccessorType;
 import javax.xml.bind.annotation.XmlElement;
@@ -26,6 +28,7 @@ import javax.xml.bind.annotation.XmlType;
  *         &lt;element ref="{http://otrcutmp4.sf.net/rss}copyright"/>
  *         &lt;element ref="{http://otrcutmp4.sf.net/rss}pubDate"/>
  *         &lt;element ref="{http://otrcutmp4.sf.net/rss}image"/>
+ *         &lt;element ref="{http://otrcutmp4.sf.net/rss}item" maxOccurs="unbounded"/>
  *       &lt;/sequence>
  *     &lt;/restriction>
  *   &lt;/complexContent>
@@ -42,7 +45,8 @@ import javax.xml.bind.annotation.XmlType;
     "language",
     "copyright",
     "pubDate",
-    "image"
+    "image",
+    "item"
 })
 @XmlRootElement(name = "channel")
 public class Channel
@@ -64,6 +68,8 @@ public class Channel
     protected PubDate pubDate;
     @XmlElement(required = true)
     protected Image image;
+    @XmlElement(required = true)
+    protected List<Item> item;
 
     /**
      * Gets the value of the title property.
@@ -259,6 +265,43 @@ public class Channel
 
     public boolean isSetImage() {
         return (this.image!= null);
+    }
+
+    /**
+     * Gets the value of the item property.
+     * 
+     * <p>
+     * This accessor method returns a reference to the live list,
+     * not a snapshot. Therefore any modification you make to the
+     * returned list will be present inside the JAXB object.
+     * This is why there is not a <CODE>set</CODE> method for the item property.
+     * 
+     * <p>
+     * For example, to add a new item, do as follows:
+     * <pre>
+     *    getItem().add(newItem);
+     * </pre>
+     * 
+     * 
+     * <p>
+     * Objects of the following type(s) are allowed in the list
+     * {@link Item }
+     * 
+     * 
+     */
+    public List<Item> getItem() {
+        if (item == null) {
+            item = new ArrayList<Item>();
+        }
+        return this.item;
+    }
+
+    public boolean isSetItem() {
+        return ((this.item!= null)&&(!this.item.isEmpty()));
+    }
+
+    public void unsetItem() {
+        this.item = null;
     }
 
 }
