9b9f6f5287b85674e0e807582fbe77a4c69d500d
thorsten-k <t.kisner@web.de>
2013-07-07 17:11:19 +0000

    Storage added (OTR-92)

---
+++ interfaces/src/main/java/net/sf/otrcutmp4/interfaces/model/Storage.java
@@ -0,0 +1,11 @@
+package net.sf.otrcutmp4.interfaces.model;
+
+import net.sf.ahtutils.interfaces.model.with.EjbWithHash;
+import net.sf.ahtutils.interfaces.model.with.EjbWithSize;
+import net.sf.ahtutils.model.interfaces.with.EjbWithId;
+import net.sf.ahtutils.model.interfaces.with.EjbWithName;
+
+public interface Storage extends EjbWithId,EjbWithName,EjbWithSize,EjbWithHash
+{
+	
+}
