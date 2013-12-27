c97077e17037a1098f7c0eb3e9906fa186b17916
thorsten-k <thorsten-k@16432bf1-0806-4b2d-aae0-48c465c8950a>
2013-12-27 12:01:31 +0000

    banner added to tv db meta (OTR-100)

---
+++ trunk/xml/src/main/java/net/sf/otrcutmp4/model/xml/tvdb/Banner.java
@@ -0,0 +1,70 @@
+
+package net.sf.otrcutmp4.model.xml.tvdb;
+
+import java.io.Serializable;
+import javax.xml.bind.annotation.XmlAccessType;
+import javax.xml.bind.annotation.XmlAccessorType;
+import javax.xml.bind.annotation.XmlAttribute;
+import javax.xml.bind.annotation.XmlRootElement;
+import javax.xml.bind.annotation.XmlType;
+
+
+/**
+ * <p>Java class for anonymous complex type.
+ * 
+ * <p>The following schema fragment specifies the expected content contained within this class.
+ * 
+ * <pre>
+ * &lt;complexType>
+ *   &lt;complexContent>
+ *     &lt;restriction base="{http://www.w3.org/2001/XMLSchema}anyType">
+ *       &lt;sequence>
+ *       &lt;/sequence>
+ *       &lt;attribute name="url" type="{http://www.w3.org/2001/XMLSchema}string" />
+ *     &lt;/restriction>
+ *   &lt;/complexContent>
+ * &lt;/complexType>
+ * </pre>
+ * 
+ * 
+ */
+@XmlAccessorType(XmlAccessType.FIELD)
+@XmlType(name = "")
+@XmlRootElement(name = "banner")
+public class Banner
+    implements Serializable
+{
+
+    private final static long serialVersionUID = 1L;
+    @XmlAttribute(name = "url")
+    protected String url;
+
+    /**
+     * Gets the value of the url property.
+     * 
+     * @return
+     *     possible object is
+     *     {@link String }
+     *     
+     */
+    public String getUrl() {
+        return url;
+    }
+
+    /**
+     * Sets the value of the url property.
+     * 
+     * @param value
+     *     allowed object is
+     *     {@link String }
+     *     
+     */
+    public void setUrl(String value) {
+        this.url = value;
+    }
+
+    public boolean isSetUrl() {
+        return (this.url!= null);
+    }
+
+}
