0a4513263359e6d46272bee35e5bb0b14549eee2
thorsten-k <t.kisner@web.de>
2013-12-28 09:26:35 +0000

    OtrCover renamed to OtrImage (OTR-101)

--- interfaces/src/main/java/net/sf/otrcutmp4/interfaces/model/Series.java
+++ interfaces/src/main/java/net/sf/otrcutmp4/interfaces/model/Series.java
@@ -6,7 +6,7 @@ import net.sf.ahtutils.model.interfaces.with.EjbWithCode;
 import net.sf.ahtutils.model.interfaces.with.EjbWithId;
 import net.sf.ahtutils.model.interfaces.with.EjbWithName;
 
-public interface Series<SERIES extends Series<SERIES,SEASON,EPISODE,COVER,STORAGE>,SEASON extends Season<SERIES,SEASON,EPISODE,COVER,STORAGE>,EPISODE extends Episode<SERIES,SEASON,EPISODE,COVER,STORAGE>,COVER extends Image,STORAGE extends Storage>
+public interface Series<SERIES extends Series<SERIES,SEASON,EPISODE,IMAGE,STORAGE>,SEASON extends Season<SERIES,SEASON,EPISODE,IMAGE,STORAGE>,EPISODE extends Episode<SERIES,SEASON,EPISODE,IMAGE,STORAGE>,IMAGE extends Image,STORAGE extends Storage>
 		extends EjbWithId,EjbWithName,EjbWithCode
 {
 	List<SEASON> getSeasons();
