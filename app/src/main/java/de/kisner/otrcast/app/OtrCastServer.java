b622d967451a70bdd8d2bfea0b36623503523580
Thorsten-K <t.kisner@web.de>
2017-04-05 11:45:29 +0200

    minor changes uploaded

--- app/src/main/java/de/kisner/otrcast/app/OtrCastServer.java
+++ app/src/main/java/de/kisner/otrcast/app/OtrCastServer.java
@@ -2,19 +2,12 @@ package de.kisner.otrcast.app;
 
 import org.eclipse.jetty.server.Handler;
 import org.eclipse.jetty.server.Server;
-import org.eclipse.jetty.server.handler.ContextHandler;
 import org.eclipse.jetty.server.handler.HandlerList;
-import org.eclipse.jetty.server.handler.ResourceHandler;
-import org.eclipse.jetty.servlet.ServletContextHandler;
-import org.eclipse.jetty.servlet.ServletHolder;
 import org.eclipse.jetty.webapp.WebAppContext;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import de.kisner.otrcast.controller.OtrCastBootstrap;
-import de.kisner.otrcast.controller.web.servlet.ImageServlet;
-import de.kisner.otrcast.controller.web.servlet.PodcastServlet;
-import de.kisner.otrcast.controller.web.servlet.StorageServlet;
 
 public class OtrCastServer 
 {
@@ -33,7 +26,7 @@ public class OtrCastServer
          ctxJsf.setParentLoaderPriority(true);            
                   
          HandlerList handlers = new HandlerList();
-         handlers.setHandlers(new Handler[] { ctxJsf});
+         handlers.setHandlers(new Handler[] {ctxJsf});
          server.setHandler(handlers);
 
          server.start();
