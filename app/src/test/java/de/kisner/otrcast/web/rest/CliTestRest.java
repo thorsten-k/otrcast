18b228240dff912d445448fffff00457b0859ffb
Thorsten-K <t.kisner@web.de>
2018-10-29 07:27:38 +0100

    changes of jeesl applied

--- app/src/test/java/de/kisner/otrcast/web/rest/CliTestRest.java
+++ app/src/test/java/de/kisner/otrcast/web/rest/CliTestRest.java
@@ -26,11 +26,13 @@ public class CliTestRest implements JeeslTestRest
 	}
 	
 	@Override public String getTime() {return rest.getTime();}
+	@Override public String getSecure() {return rest.getSecure();}
 	
 	public static void main(String[] args) throws Exception
 	{
 		Configuration config = OtrCastBootstrap.init();	
 		CliTestRest rest = new CliTestRest(config);
 		logger.info(rest.getTime());
+		logger.info(rest.getSecure());
 	}
 }
