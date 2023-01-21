ede6e216c29a43259ae5066403107aa57ca38993
Thorsten-K <t.kisner@web.de>
2023-01-21 15:44:07 +0200

    TestRest updated with JSON

--- app/src/test/java/de/kisner/otrcast/web/rest/CliTestRest.java
+++ app/src/test/java/de/kisner/otrcast/web/rest/CliTestRest.java
@@ -6,6 +6,8 @@ import org.jboss.resteasy.client.jaxrs.ResteasyClient;
 import org.jboss.resteasy.client.jaxrs.ResteasyClientBuilder;
 import org.jboss.resteasy.client.jaxrs.ResteasyWebTarget;
 import org.jeesl.api.rest.rs.system.JeeslTestRest;
+import org.jeesl.model.json.io.ssi.update.JsonSsiUpdate;
+import org.jeesl.model.json.system.job.JsonJob;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -27,6 +29,8 @@ public class CliTestRest implements JeeslTestRest
 	
 	@Override public String dateTimePublic() {return rest.dateTimePublic();}
 	@Override public String dateTimeRestricted() {return rest.dateTimeRestricted();}
+	@Override public JsonSsiUpdate jsonUpdate() {return rest.jsonUpdate();}
+	@Override public JsonJob jsonJob() {return rest.jsonJob();}
 	
 	public static void main(String[] args) throws Exception
 	{
