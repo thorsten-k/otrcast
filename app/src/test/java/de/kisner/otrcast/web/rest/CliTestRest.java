39c69dd17f6c71cfdd74caf92fd18f804e08b12f
Thorsten-K <t.kisner@web.de>
2023-01-21 15:59:54 +0200

    JsonJob renamed to JsonSystemJob

--- app/src/test/java/de/kisner/otrcast/web/rest/CliTestRest.java
+++ app/src/test/java/de/kisner/otrcast/web/rest/CliTestRest.java
@@ -7,7 +7,7 @@ import org.jboss.resteasy.client.jaxrs.ResteasyClientBuilder;
 import org.jboss.resteasy.client.jaxrs.ResteasyWebTarget;
 import org.jeesl.api.rest.rs.system.JeeslTestRest;
 import org.jeesl.model.json.io.ssi.update.JsonSsiUpdate;
-import org.jeesl.model.json.system.job.JsonJob;
+import org.jeesl.model.json.system.job.JsonSystemJob;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -30,7 +30,7 @@ public class CliTestRest implements JeeslTestRest
 	@Override public String dateTimePublic() {return rest.dateTimePublic();}
 	@Override public String dateTimeRestricted() {return rest.dateTimeRestricted();}
 	@Override public JsonSsiUpdate jsonUpdate() {return rest.jsonUpdate();}
-	@Override public JsonJob jsonJob() {return rest.jsonJob();}
+	@Override public JsonSystemJob jsonJob() {return rest.jsonJob();}
 	
 	public static void main(String[] args) throws Exception
 	{
