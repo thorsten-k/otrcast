846e20602cf0c1d3861cedeff229d07ecb9f2cd1
Thorsten-K <t.kisner@web.de>
2023-09-21 09:06:35 +0200

    TestRest improved

--- app/src/test/java/de/kisner/otrcast/web/rest/CliTestRest.java
+++ app/src/test/java/de/kisner/otrcast/web/rest/CliTestRest.java
@@ -5,19 +5,21 @@ import org.jboss.resteasy.client.jaxrs.BasicAuthentication;
 import org.jboss.resteasy.client.jaxrs.ResteasyClient;
 import org.jboss.resteasy.client.jaxrs.ResteasyClientBuilder;
 import org.jboss.resteasy.client.jaxrs.ResteasyWebTarget;
-import org.jeesl.api.rest.rs.system.JeeslTestRest;
+import org.jeesl.api.rest.i.system.JeeslTestRestInterface;
+import org.jeesl.api.rest.rs.jx.system.JeeslTestRest;
 import org.jeesl.model.json.io.ssi.update.JsonSsiUpdate;
 import org.jeesl.model.json.system.job.JsonSystemJob;
+import org.jeesl.model.json.util.JsonTime;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import de.kisner.otrcast.controller.OtrCastBootstrap;
 
-public class CliTestRest implements JeeslTestRest
+public class CliTestRest implements JeeslTestRestInterface
 {
 	final static Logger logger = LoggerFactory.getLogger(CliTestRest.class);
 	
-	private JeeslTestRest rest;
+	private JeeslTestRestInterface rest;
 	
 	public CliTestRest(Configuration config)
 	{
@@ -31,7 +33,8 @@ public class CliTestRest implements JeeslTestRest
 	@Override public String dateTimeRestricted() {return rest.dateTimeRestricted();}
 	@Override public JsonSsiUpdate jsonUpdate() {return rest.jsonUpdate();}
 	@Override public JsonSystemJob jsonJob() {return rest.jsonJob();}
-	@Override public String jsonMirror(String content) {return rest.jsonMirror(content);}
+	@Override public JsonTime jsonTimeDownload() {return rest.jsonTimeDownload();}
+	@Override public JsonTime jsonTimeUpload(JsonTime time) {return rest.jsonTimeUpload(time);}
 	
 	public static void main(String[] args) throws Exception
 	{
